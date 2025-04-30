f6e91bf254c4df8827b40c98fd23883fa15e3568
shaaibu7 <shaaibusuleiman9@gmail.com>
2025-04-30 13:05:35 +0100

    chore: code refactor

--- backend/contracts/WalletX.sol
+++ backend/contracts/WalletX.sol
@@ -5,6 +5,8 @@ pragma solidity ^0.8.28;
 // import "hardhat/console.sol";
 
 import './interfaces/IERC20.sol';
+import './lib/Errors.sol';
+
 
 contract WalletX {
     address tokenAddress;
@@ -19,10 +21,12 @@ contract WalletX {
         string walletName;
         bool active;
         uint walletId;
+        uint walletBalance;
     }
 
     struct WalletMember {
         address memberAddress;
+        string organizationName;
         string name;
         bool active;
         uint256 spendLimit; 
@@ -41,46 +45,75 @@ contract WalletX {
         _;
     }
 
-    function registerWallet(string memory _walletName) external {
+    function registerWallet(string memory _walletName, uint256 _fundAmount) external {
         require(walletAdmin[msg.sender].active != true, "Cannot create multiple wallets with one wallet address");
+
+        // fund escrow with funds
+        uint256 allowance = IERC20(tokenAddress).allowance(msg.sender, address(this));
+        require(allowance == _fundAmount, "No allowance to spend funds at the moment");
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _fundAmount);
+
+
         WalletOrganisation memory walletOrganisation = WalletOrganisation({
             walletName: _walletName,
             active: true,
-            walletId: walletIdTrack
+            walletId: walletIdTrack,
+            walletBalance: _fundAmount
         });
+
+        walletIdTrack += 1;
 
         walletAdmin[msg.sender] = walletOrganisation;
 
     }
 
     function onboardMembers(address _memberAddress, string memory _memberName, uint256 _fundAmount, uint256 _memberIdentifier) onlyAdmin external {
+        string memory _organizationName = walletAdmin[msg.sender].walletName;
+        uint walletBalance = walletAdmin[msg.sender].walletBalance;
+
+        if (walletBalance < _fundAmount) {
+            revert Error.InsufficientFunds();
+        }
+
+        
         WalletMember memory member = WalletMember({
             memberAddress: _memberAddress,
+            organizationName: _organizationName,
             name: _memberName,
             active: true,
             spendLimit: _fundAmount,
             memberIdentifier: _memberIdentifier
         });
 
-        uint adminBalance = IERC20(tokenAddress).balanceOf(msg.sender);
-
-        if (adminBalance > _fundAmount) {
-            IERC20(tokenAddress).approve(_memberAddress, _fundAmount);
-        }
-
         walletMember[_memberAddress] = member;
         walletOrganisationMembers[msg.sender].push(member);
 
     }
 
+    function reimburseOrganization(uint256 _amount) external onlyAdmin {
+        // fund escrow with funds
+        uint256 allowance = IERC20(tokenAddress).allowance(msg.sender, address(this));
+        require(allowance == _amount, "No allowance to spend funds at the moment");
+        IERC20(tokenAddress).transferFrom(msg.sender, address(this), _amount);
+
+
+        walletAdmin[msg.sender].walletBalance += _amount;
+
+    }
+
     function reimburseMember(uint256 _memberIdentifier, uint256 _amount) external onlyAdmin {
 
-        WalletMember[] memory members = walletOrganisationMembers[msg.sender];
+        uint walletBalance = walletAdmin[msg.sender].walletBalance;
 
-        for(uint256 i = 0; i < walletOrganisationMembers[msg.sender].length; i++) {
+        if (walletBalance < _amount) {
+            revert Error.InsufficientFunds();
+        }
+
+        WalletMember[] storage members = walletOrganisationMembers[msg.sender];
+
+        for(uint256 i = 0; i < members.length; i++) {
             if (members[i].memberIdentifier == _memberIdentifier) {
                 members[i].spendLimit += _amount;
-                IERC20(tokenAddress).approve(members[i].memberAddress, _amount);
             }
         }
     }
